a399821a857808392df1a680d1ce8f77e6dd3a80
Benjamin Whipp <ben@benwhipp.dev>
2019-11-09 11:30:28 +0000

    Tweaks to dark mode, initial mobile responsive changes

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ const addDarkMode = () => {
     document.querySelector('h1').style.color = '#fff';
     document.querySelector('h2').style.color = '#fff';
     document.querySelector('p').style.color = '#fff';
-    document.querySelector('.aboutUSPs').style.color = '#fff';
+    document.getElementById('aboutUSPs').style.color = '#fff';
     document.getElementById('circle').style.clipPath = 'circle(100%)';
     document.getElementById('circle').style.opacity = 1;
+    document.querySelector('nav').style.background = '#352D39';
+    document.querySelector('body').style.color = '#fff';
+    document.getElementById('projects').style.background = '#6E6362';
+    darkModeToggle.style.background = '#6E6362';
+    const changeAboutH2s = () => {
+        document.getElementById('aboutUSPs').querySelectorAll('h2').forEach(el => {
+            el.style.color = '#fff';
+        });
+    }
+    changeAboutH2s();
     isDarkMode = true;
 };
 
@@ -16,9 +26,19 @@ const revertDarkMode = () => {
     document.querySelector('h1').style.color = '#352D39';
     document.querySelector('h2').style.color = '#F9AC52';
     document.querySelector('p').style.color = '#352D39';
-    document.querySelector('.aboutUSPs').style.color = '#352D39';
+    document.getElementById('aboutUSPs').style.color = '#352D39';
     document.getElementById('circle').style.clipPath = 'circle(5% at 0% 0%)';
     document.getElementById('circle').style.opacity = 0;
+    document.querySelector('nav').style.background = '#fff';
+    document.querySelector('body').style.color = '#352D39';
+    document.getElementById('projects').style.background = '#f8f8f8';
+    darkModeToggle.style.background = '#352D39';
+    const changeAboutH2s = () => {
+        document.getElementById('aboutUSPs').querySelectorAll('h2').forEach(el => {
+            el.style.color = '#352D39';
+        });
+    }
+    changeAboutH2s();
     isDarkMode = false;
 };
 
